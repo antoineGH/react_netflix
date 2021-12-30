33860ee761dfd2c7bd328fe529b49cdf398d8e87
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-30 22:25:08 +0800

    remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from 'antd'
 import AuthApp from 'layouts/AuthApp'
 import UnAuthApp from 'layouts/UnAuthApp'
@@ -7,29 +6,19 @@ import './App.css'
 
 function App() {
   const { Header, Content, Footer } = Layout
-  const logged = true
+  const logged = false
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Layout>
-          <Header>Header</Header>
-        </Layout>
-        <Layout>
-          <Content>
-            <Routes>
-              {logged ? (
-                <Route index element={<AuthApp />} />
-              ) : (
-                <Route index element={<UnAuthApp />} />
-              )}
-            </Routes>
-          </Content>
-        </Layout>
-        <Layout>
-          <Footer>Footer</Footer>
-        </Layout>
-      </BrowserRouter>
+      <Layout>
+        <Header>Header</Header>
+      </Layout>
+      <Layout>
+        <Content></Content>
+      </Layout>
+      <Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     </div>
   )
 }
