90a24510dce90ba73f52733c4d162d2ca3db28b5
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-31 03:31:43 +0800

    update route with movie tv new pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,16 +3,22 @@ import UnAuthApp from 'layouts/UnAuthApp'
 import Login from 'pages/loginPage/LoginPage'
 import Register from 'pages/registerPage/RegisterPage'
 import AuthApp from 'layouts/AuthApp'
-import Browse from 'pages/browsePage/BrowsePage'
-import Account from 'pages/accountPage/AccountPage'
+import BrowsePage from 'pages/browsePage/BrowsePage'
+import MoviePage from 'pages/moviePage/MoviePage'
+import TvPage from 'pages/tvPage/TvPage'
+import NewPopularPage from 'pages/newPopularPage/NewPopularPage'
+import AccountPage from 'pages/accountPage/AccountPage'
 
 const routes = (isLoggedIn: Boolean) => [
   {
     path: '/auth',
     element: isLoggedIn ? <AuthApp /> : <Navigate to="/login" />,
     children: [
-      { path: 'browse', element: <Browse /> },
-      { path: 'account', element: <Account /> },
+      { path: 'browse', element: <BrowsePage /> },
+      { path: 'movies', element: <MoviePage /> },
+      { path: 'tvs', element: <TvPage /> },
+      { path: 'news', element: <NewPopularPage /> },
+      { path: 'account', element: <AccountPage /> },
       { path: '/auth', element: <Navigate to="/auth/browse" /> },
     ],
   },
