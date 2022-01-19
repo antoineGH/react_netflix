d10745551a9718359e0745e70f9244bec37d0da1
Antoine Ratat <antoine.ratat@gmail.com>
2022-01-19 15:49:31 +0800

    useAuth define on update token

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -6,9 +6,9 @@ export const { useAuth, authFetch, login, logout } =
   createAuthProvider<Session>({
     getAccessToken: session => session.access_token,
     storage: localStorage,
-    // onUpdateToken: token =>
-    //   fetch('/update-token', {
-    //     method: 'POST',
-    //     body: token.refreshToken,
-    //   }).then(r => r.json()),
+    onUpdateToken: token =>
+      fetch('/api/refresh', {
+        method: 'POST',
+        body: token.refreshToken,
+      }).then(r => r.json()),
   })
