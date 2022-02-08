1a9cc50b521466ac8fcdd07076da00f207bc87da
Antoine Ratat <antoine.ratat@gmail.com>
2022-02-08 19:02:57 +0800

    handle update rename delete list

--- src/components/modalList/ModalList.tsx
+++ src/components/modalList/ModalList.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react'
-import { Modal, Button, Alert } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { Modal, Button, Alert, Form, Input } from 'antd'
 import { List, Lists } from 'types/list'
 import { useAppDispatch, useAppSelector } from 'hooks/hooks'
 import {
@@ -22,11 +22,15 @@ interface props {
 const ModalList = ({ list, count, lists, visible, setVisible }: props) => {
   const dispatch = useAppDispatch()
   const [error, setError] = useState<string | null>(null)
-
+  const [listToUpdate, setListToUpdate] = useState(list.list_title)
   const isLoadingUpdateList = useAppSelector(updateListLoadingSelector)
   const hasErrorUpdateList = useAppSelector(updateListErrorSelector)
   const isLoadingDeleteList = useAppSelector(deleteListLoadingSelector)
   const hasErrorDeleteList = useAppSelector(deleteListErrorSelector)
+
+  const [form] = Form.useForm()
+
+  useEffect(() => form.resetFields(), [list.list_title, form])
 
   useEffect(() => {
     if (hasErrorUpdateList) {
@@ -42,6 +46,22 @@ const ModalList = ({ list, count, lists, visible, setVisible }: props) => {
     }
   }, [hasErrorUpdateList, hasErrorDeleteList, error])
 
+  useEffect(() => {
+    setListToUpdate(list.list_title)
+    form.setFieldsValue({
+      list: list.list_title,
+    })
+  }, [list.list_title, form])
+
+  const deleteList = (listID: number) => {
+    if (lists.length <= 1) {
+      setError('Impossible to delete default list')
+      return
+    }
+    dispatch(removeList(listID))
+    setVisible(false)
+  }
+
   const putList = (listID: number, newList: string) => {
     let hasExistingList = false
     lists.forEach(list => {
@@ -54,35 +74,66 @@ const ModalList = ({ list, count, lists, visible, setVisible }: props) => {
       return
     }
     dispatch(updateList({ listTitle: newList, listID }))
-    setVisible(false)
+    setListToUpdate('')
+    setTimeout(() => {
+      setVisible(false)
+    }, 700)
   }
-  const deleteList = (listID: number) => {
-    if (lists.length <= 1) {
-      setError('Impossible to delete default list')
-      return
-    }
-    dispatch(removeList(listID))
-    setVisible(false)
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    setListToUpdate(e.currentTarget.value)
   }
 
   return (
     <Modal
-      title="Modal"
+      getContainer={false}
+      title="List Settings"
       centered
       visible={visible}
-      onOk={() => setVisible(false)}
+      okText="Update"
+      okButtonProps={{
+        loading: isLoadingUpdateList,
+      }}
+      onOk={() => putList(list.list_id, listToUpdate)}
       onCancel={() => setVisible(false)}
       width={1000}
     >
       {error && <Alert message={error} type="error" />}
       <p>{list.list_title}</p>
 
-      <Button
-        loading={isLoadingUpdateList}
-        onClick={() => putList(list.list_id, 'new list name updated')}
+      <Form
+        form={form}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        autoComplete="off"
       >
-        Rename
-      </Button>
+        <Form.Item
+          label="List"
+          name="list"
+          rules={[
+            { required: true, message: 'Please input your list name' },
+            {
+              pattern: /^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/,
+              message: "List name shouldn't contain spaces or numbers",
+            },
+            {
+              min: 3,
+              message: 'List name should contain at least 3 characters',
+            },
+            { max: 25, message: "List name shouldn't exceed 25 characters" },
+          ]}
+        >
+          <Input
+            id="input"
+            name="input"
+            onChange={handleChange}
+            placeholder={listToUpdate}
+            disabled={isLoadingUpdateList}
+            onPressEnter={() => putList(list.list_id, listToUpdate)}
+          />
+        </Form.Item>
+      </Form>
       {count !== 1 && (
         <Button
           loading={isLoadingDeleteList}
