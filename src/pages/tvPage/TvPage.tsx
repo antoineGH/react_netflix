5d4706fe30e33a61937eb793a88606e47e8a1316
Antoine Ratat <antoine.ratat@gmail.com>
2022-02-09 18:05:56 +0800

    remove test selector from tvpage

--- src/pages/tvPage/TvPage.tsx
+++ src/pages/tvPage/TvPage.tsx
@@ -3,41 +3,28 @@ import { useEffect } from 'react'
 import { useAppSelector } from 'hooks/hooks'
 import { getTrendingSelector } from 'reducers/trending'
 import { getGenreSelector } from 'reducers/genres'
-import { getFindIDSelector } from 'reducers/find'
+
 import { getExternalSelector } from 'reducers/external'
 import { getDiscoverSelector } from 'reducers/discover'
 import { getCreditsSelector } from 'reducers/credits'
 import { getCreditDetailsSelector } from 'reducers/creditdetails'
-import { getCountriesSelector } from 'reducers/configurations'
-import { getAccountSelector } from 'reducers/account'
-import { getListsSelector } from 'reducers/list'
-import { getListSelector } from 'reducers/list'
 
 const TvPage = () => {
   useDocumentTitle('TV Shows')
   const trending = useAppSelector(getTrendingSelector)
   const genres = useAppSelector(getGenreSelector)
-  const findID = useAppSelector(getFindIDSelector)
   const external = useAppSelector(getExternalSelector)
   const discover = useAppSelector(getDiscoverSelector)
   const credits = useAppSelector(getCreditsSelector)
   const creditdetails = useAppSelector(getCreditDetailsSelector)
-  const countries = useAppSelector(getCountriesSelector)
-  const account = useAppSelector(getAccountSelector)
-  const lists = useAppSelector(getListsSelector)
-  const list = useAppSelector(getListSelector)
+
   useEffect(() => {
     console.log(trending)
     console.log(genres)
-    console.log(findID)
     console.log(external)
     console.log(discover)
     console.log(credits)
     console.log(creditdetails)
-    console.log(countries)
-    console.log(account)
-    console.log(lists)
-    console.log(list)
   }, [])
   return <p>TvPage</p>
 }
